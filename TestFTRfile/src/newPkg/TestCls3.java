18247577bbe49ecedeee45720755d02e8bb2026b
NIKHIL <NIKHIL@ASUS-UX430-NG>
2021-03-03 16:06:18 +0530

    test new changes

---
+++ TestFTRfile/src/newPkg/TestCls3.java
@@ -0,0 +1,9 @@
+package newPkg;
+
+public class TestCls3 {
+	
+	public static void test() {
+		System.out.println("Test");
+	}
+
+}
--- TestFTRfile/src/testFTRpkg/TestCls.java
+++ TestFTRfile/src/testFTRpkg/TestCls.java
@@ -1,10 +1,13 @@
 package testFTRpkg;
 
+import newPkg.TestCls3;
+
 public class TestCls {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hI vAIBHAV");
+TestCls3.test();
 	}
 
 }
